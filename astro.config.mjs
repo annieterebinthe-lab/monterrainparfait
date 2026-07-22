b8fefb941a65ea7b5c8081bb4938f4f0b7c11e72
Alexandre Charbonne <annieterebinthe@gmail.com>
2026-07-22 14:39:31 +0200

    Version 1.0 - Première mise en ligne

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://www.monterrainparfait.fr',
+
+  integrations: [
+    sitemap()
+  ],
+
+  vite: {
+    plugins: [tailwind()]
+  }
+});
